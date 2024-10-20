66f2c125b0dec518192e140e792637a5fd1b9924
wersomd <wersomd.dev@gmail.com>
2024-10-20 17:04:46 +0500

    Desktop version end

--- js/main.js
+++ js/main.js
@@ -9,3 +9,32 @@ document.addEventListener("DOMContentLoaded", function () {
     }, index * 200);
   });
 });
+
+const menuLinks = document.querySelectorAll(".menu-link[data-goto]");
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        120 -
+        document.querySelector("header").offsetHeight;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+
+      e.preventDefault();
+    }
+  }
+}
